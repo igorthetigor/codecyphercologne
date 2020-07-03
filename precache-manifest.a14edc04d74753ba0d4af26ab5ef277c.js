6085f6d771278dd3003904372f86236995da4442
igorthetigor <igor.liverant@gmail.com>
2020-07-03 22:47:41 +0200

    Updates

---
+++ precache-manifest.a14edc04d74753ba0d4af26ab5ef277c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db0c5b86fb01602bb172d0532001eb2",
+    "url": "/codecyphercologne/index.html"
+  },
+  {
+    "revision": "aa3482c27ad716d1b23a",
+    "url": "/codecyphercologne/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5e7d006c3d451b7a56f",
+    "url": "/codecyphercologne/static/js/2.d22b4a7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codecyphercologne/static/js/2.d22b4a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3482c27ad716d1b23a",
+    "url": "/codecyphercologne/static/js/main.48a1e134.chunk.js"
+  },
+  {
+    "revision": "15620a0560cee81310be",
+    "url": "/codecyphercologne/static/js/runtime-main.0f29aaf6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/codecyphercologne/static/media/logo.5d5d9eef.svg"
+  }
+]);
